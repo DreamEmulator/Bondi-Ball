d0aa3d9a516cb54eb1e990342355c46b41142168
Sebastiaan Hols <sebastiaan@q42.nl>
2023-02-15 00:00:41 +0100

    ScoreVC background

--- Sources/ViewControllers/Game/Game+Sounds.swift
+++ Sources/ViewControllers/Game/Game+Sounds.swift
@@ -18,11 +18,7 @@ extension GameVC {
       try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
       try AVAudioSession.sharedInstance().setActive(true)
 
-      /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
       player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
-
-      /* iOS 10 and earlier require the following line:
-       player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
 
       guard let player = player else { return }
 
