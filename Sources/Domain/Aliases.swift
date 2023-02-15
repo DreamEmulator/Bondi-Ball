4cb643b31c80e5d8923f362a43af1ea332b47640
Sebastiaan Hols <sebastiaan@q42.nl>
2023-02-15 21:40:09 +0100

    Clean up

--- Sources/Domain/Aliases.swift
+++ Sources/Domain/Aliases.swift
@@ -3,11 +3,8 @@
 //  Gestures-In-Fluid-Interfaces
 //
 //  Created by Sebastiaan Hols on 07/01/2023.
-//  Copyright © 2023 Christian Schnorr. All rights reserved.
 //
 
 import Foundation
 
 typealias AnonymousClosure = () -> Void
-
-
